3ef4836e42b928fe48177006ab5751deb8af8c97
Mark Vousden <m.vousden@soton.ac.uk>
2019-11-20 17:14:48 +0000

    Some breaking changes for the purposes of debugging.

--- generator/fragments/fd_point_rts.c
+++ generator/fragments/fd_point_rts.c
@@ -3,6 +3,8 @@ if (deviceState->did_we_just_update || deviceState->is_initialised == 0)
     *readyToSend |= RTS_FLAG_state_push;
 
     /* Send to the supervisor every eight time steps. */
-    if (((deviceState->iteration >> 3) << 3) == deviceState->iteration)
+    //if (((deviceState->iteration >> 3) << 3) == deviceState->iteration)
+    /* Send to the supervisor if I'm x0=0. */
+    if (deviceProperties->x0 == 0)
         *readyToSend |= RTS_FLAG_exfiltrate;
 }
