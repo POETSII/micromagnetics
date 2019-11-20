1aadc2fa3349f8b8287e8138001e6a9b7fa87f8d
Mark Vousden <m.vousden@soton.ac.uk>
2019-11-20 11:58:34 +0000

    Complain about bad templater arguments.

--- generator/template_executable.c
+++ generator/template_executable.c
@@ -8,6 +8,15 @@ int main(int argc, char** argv)
     FILE* outFile;
     int rc;
 
+    /* Complain about bad arguments. */
+    if (argc != 3)
+    {
+        fprintf(stderr,
+                "Argument 1: Input template. Argument 2: Destination. No "
+                "more, no less.\n");
+        return 1;
+    }
+
     /* Open the input and output files. */
     inFile = fopen(argv[1], "rb+");
     if (inFile == NULL)
