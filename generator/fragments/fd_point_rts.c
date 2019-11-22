b090eb124ee23600f594fdefb46309239cfbc451
Mark Vousden <m.vousden@soton.ac.uk>
2019-11-22 18:48:01 +0000

    Catch up with master.

--- generator/fragments/fd_point_rts.c
+++ generator/fragments/fd_point_rts.c
@@ -3,9 +3,7 @@ if (deviceState->did_we_just_update || deviceState->is_initialised == 0)
     *readyToSend |= RTS_FLAG_state_push;
 
     /* Send to the supervisor every eight time steps. */
-    //if (((deviceState->iteration >> 3) << 3) == deviceState->iteration)
-    /* Send to the supervisor if I'm x0=0. */
-    if (deviceProperties->x0 == 0)
+    if (((deviceState->iteration >> 3) << 3) == deviceState->iteration)
         *readyToSend |= RTS_FLAG_exfiltrate;
 
     /* Send to the supervisor only if we're device zero.
