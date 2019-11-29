65ba47a1de8e1bc8306a7c3a494bd9a9c9aa5509
Mark Vousden <m.vousden@soton.ac.uk>
2019-11-29 08:12:53 +0000

    More comments!

--- generator/fragments/check_increment_state_2d.c
+++ generator/fragments/check_increment_state_2d.c
@@ -1,30 +1,38 @@
 /* Now we need to check if our neighbours satisfy the update condition. If they
  * do, we update our state. */
-uint8_t goingToUpdate;
-float h_eff_dt_x0;
-float h_eff_dt_x1;
-float h_eff_dt_x2;
-float m_x0_x0plus;
-float m_x1_x0plus;
-float m_x2_x0plus;
-float m_x0_x0minus;
+
+uint8_t goingToUpdate;  /* 1 if our neighbours' iteration is >= ours. */
+
+float h_eff_dt_x0;  /* Effective field, multiplied by time differential... */
+float h_eff_dt_x1;  /* ...in the x0, x1, and x2 directions. Signage is... */
+float h_eff_dt_x2;  /* ...questionable. */
+
+float m_x0_x0plus;  /* The x0 (or x1 or x2) -component of the magnetic... */
+float m_x1_x0plus;  /* ...moment field value at the neighbour one ahead... */
+float m_x2_x0plus;  /* ...in the x0 direction in the domain. */
+
+float m_x0_x0minus;  /* As above, but for one behind in the x0 direction. */
 float m_x1_x0minus;
 float m_x2_x0minus;
-float m_x0_x1plus;
+
+float m_x0_x1plus;  /* As above, but for one ahead in the x1 direction. */
 float m_x1_x1plus;
 float m_x2_x1plus;
-float m_x0_x1minus;
+
+float m_x0_x1minus;  /* As above, but for one behind in the x1 direction. */
 float m_x1_x1minus;
 float m_x2_x1minus;
-float dm_x0;
-float dm_x1;
-float dm_x2;
+
+float dm_x0;  /* The magnetic moment field differential for this... */
+float dm_x1;  /* iteration, derived from Euler updating, in the x0, x1, and */
+float dm_x2;  /* x2 directions in the codomain.
+
 float m_dot_h_dt;
 float target;
 union {
     float f;
     uint32_t i;
-} fisr;
+} fisr;  /* For fast-inverse square root. */
 
 goingToUpdate = 1;  /* Innocent until proven guilty. */
 if (deviceState->iteration_x0plus < deviceState->iteration) goingToUpdate = 0;
