68c98ff2b350c09d7160ed9c35d4c93b8805c890
Mark Vousden <m.vousden@soton.ac.uk>
2019-12-05 13:47:07 +0000

    Better notebook-fu

--- generator/micromagnetics_notebook_utils.py
+++ generator/micromagnetics_notebook_utils.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
+import ctypes
 import os
+
+generatorLibPath = "./libgenerator.so"
 
 # If you're importing this without the generator being built, we're going to
 # complain.
-if not os.path.exists("../generator"):
-    raise ImportError("Import failed: Be sure to build the generator before "
-                      "importing this library.")
+if not os.path.exists(generatorLibPath):
+    raise ImportError("Be sure to build the generator before importing this "
+                      "library.")
 
-def generate():
-    pass
+# Grab the generator library.
+generator = ctypes.CDLL(generatorLibPath)
 
+# Expose the method elegantly.
+def generate(x0Max, x1Max, outPath):
+    if outPath == "":
+        output = "unnamed_micromagnetic_simulation.xml"
+    generator.do_it(x0Max, x1Max, outPath.encode("UTF-8"))
+    print("XML generated! It is at '{}'".format(os.path.abspath(outPath)))
 
 if __name__ == "__main__":
     print("This is a library designed to be imported by the generator and "
--- generator/readme.md
+++ generator/readme.md
@@ -5,3 +5,9 @@ C89
 
 The templating mechanism uses POSIX' basename method, so if you're on Windows,
 you'll need to code this in yourself.
+
+Notebook
+===
+
+The JuPyter notebook is a flashy way of generating XML. To enable widgets:
+https://ipywidgets.readthedocs.io/en/latest/user_install.html
