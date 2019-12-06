06aa958eebe13643e5f2e32e70ed312ba3a9fc03
Mark Vousden <m.vousden@soton.ac.uk>
2019-12-06 14:09:14 +0000

    Only exfiltrate the final data point.

--- generator/fragments/fd_point_rts.c
+++ generator/fragments/fd_point_rts.c
@@ -2,11 +2,17 @@ if (deviceState->did_we_just_update || deviceState->is_initialised == 0)
 {
     *readyToSend |= RTS_FLAG_state_push;
 
-    /* Send to the supervisor every eight time steps. */
-    if (((deviceState->iteration >> 3) << 3) == deviceState->iteration)
-        *readyToSend |= RTS_FLAG_exfiltrate;
+    /* Exfiltration modes, either:
+     *  - send to the supervisor every eight time steps.
+     *  - send to the supervisor only if we're device zero.
+     *  - send to the supervisor only if we've hit the final state.
+     *
+     * Using one of these predicates:
+     *
+     *  - if (((deviceState->iteration >> 3) << 3) == deviceState->iteration)
+     *  - if (deviceProperties->x0 == 0)
+     *  - if (deviceState->iteration >= deviceProperties->finishLine) */
 
-    /* Send to the supervisor only if we're device zero.
-    if (deviceProperties->x0 == 0) *readyToSend |= RTS_FLAG_exfiltrate;
-    */
+    if (deviceState->iteration >= deviceProperties->finishLine)
+        *readyToSend |= RTS_FLAG_exfiltrate
 }
