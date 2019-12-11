3622e24b28503a90ef7ddef57ecd9edc82b91809
Mark Vousden <m.vousden@soton.ac.uk>
2019-12-11 22:05:58 +0000

    Better notebook.

--- generator/micromagnetics_notebook_utils.py
+++ generator/micromagnetics_notebook_utils.py
@@ -2,6 +2,9 @@
 
 import ctypes
 import os
+import sys
+sys.path.append("../postprocessing/")
+import postprocess
 
 generatorLibPath = "./libgenerator.so"
 
@@ -15,11 +18,34 @@ if not os.path.exists(generatorLibPath):
 generator = ctypes.CDLL(generatorLibPath)
 
 # Expose the method elegantly.
-def generate(x0Max, x1Max, outPath):
-    if outPath == "":
-        output = "unnamed_micromagnetic_simulation.xml"
+def generate(x0Max, x1Max):
+    print("Generating XML...")
+    outPath = "micromagnetics.xml"
     generator.do_it(x0Max, x1Max, outPath.encode("UTF-8"))
-    print("XML generated! It is at '{}'".format(os.path.abspath(outPath)))
+    print("XML generated in '{}'!".format(os.path.abspath(outPath)))
+
+# Deploy to a POETS box.
+def deploy(xmlPath="micromagnetics.xml", boxName="ayres"):
+    print("Deploying to POETS engine \"{}\"...".format(boxName))
+    os.system("scp \"{}\" {}:~/repos/orchestrator/application_staging/xml/"
+              .format(xmlPath, boxName))
+    print("Application deployed.")
+
+# Run on POETS.
+def run(boxName="ayres"):
+    os.system("ssh {}-script -- \"bash run_demo.sh\"")
+
+# Extract results.
+def exfiltrate(boxName="ayres"):
+    print("Exfiltrating simulation results from POETS engine \"{}\"..."
+          .format(boxName))
+    os.system("scp {}:~/repos/orchestrator/bin/micromagnetics* ./"
+              .format(boxName))
+    print("Results exfiltrated.")
+
+# Visualise
+def run_my_shit():
+    postprocess.doit()
 
 if __name__ == "__main__":
     print("This is a library designed to be imported by the generator and "
