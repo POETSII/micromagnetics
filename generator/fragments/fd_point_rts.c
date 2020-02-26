ec7d898eecc90d004ced1ee3b0a1a7b34d8c77f8
Mark Vousden <m.vousden@soton.ac.uk>
2020-02-26 22:42:44 +0000

    Some slight rearrangement.

--- generator/fragments/fd_point_rts.c
+++ generator/fragments/fd_point_rts.c
@@ -2,6 +2,11 @@ if (deviceState->did_we_just_update || deviceState->is_initialised == 0)
 {
     *readyToSend |= RTS_FLAG_state_push;
 
+    /* Starts the timer (device "0" only). */
+    if (deviceState->iteration <= 4 &&
+        deviceProperties->x0 == 0 && deviceProperties->x1 == 0)
+        *readyToSend |= RTS_FLAG_exfiltrate;
+
     /* Exfiltration modes, either:
      *  - send to the supervisor the first iteration.
      *  - send to the supervisor every eight time steps.
@@ -14,11 +19,6 @@ if (deviceState->did_we_just_update || deviceState->is_initialised == 0)
      *  - if (((deviceState->iteration >> 3) << 3) == deviceState->iteration)
      *  - if (deviceProperties->x0 == 0 && deviceProperties->x1 == 0)
      *  - if (deviceState->iteration >= deviceProperties->finishLine) */
-
-    /* Starts the timer (device "0" only). */
-    if (deviceState->iteration <= 4 &&
-        deviceProperties->x0 == 0 && deviceProperties->x1 == 0)
-        *readyToSend |= RTS_FLAG_exfiltrate;
 
     /* Exfiltrates (iteration > some value) */
     if (deviceState->done == 0 &&
